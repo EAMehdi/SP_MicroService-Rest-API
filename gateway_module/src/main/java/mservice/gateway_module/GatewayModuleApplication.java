6413af36684dd997de90c5c0a3fb1b776b4d9b87
MEHDI EA <mehdi.el-ayadi@dauphine.eu>
2023-04-17 00:17:02 +0200

    Gateaway update

--- gateway_module/src/main/java/mservice/gateway_module/GatewayModuleApplication.java
+++ gateway_module/src/main/java/mservice/gateway_module/GatewayModuleApplication.java
@@ -2,6 +2,8 @@ package mservice.gateway_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 
 @SpringBootApplication
@@ -11,4 +13,14 @@ public class GatewayModuleApplication {
 		SpringApplication.run(GatewayModuleApplication.class, args);
 	}
 
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
 }
+
+
